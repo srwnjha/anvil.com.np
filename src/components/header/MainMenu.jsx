b3464f86d05013dd8c31f5e4f8d53430de0b83f7
Pramit Amatya <pramitamatya786@gmail.com>
2026-05-24 23:22:58 +0545

    ok

--- src/components/header/MainMenu.jsx
+++ src/components/header/MainMenu.jsx
@@ -1,51 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const MainMenu = () => {
-
   return (
     <ul>
-      <li className="menu-item-has-children">
+      <li>
         <Link to="/">Home</Link>
-        <ul className="sub-menu">
+        {/* <ul className="sub-menu">
           <li><Link to="/">Home One</Link></li>
           <li><Link to="/home-two">Home Two</Link></li>
           <li><Link to="/home-three">Home Three</Link></li>
-        </ul>
+        </ul> */}
       </li>
 
-      <li><Link to="/about">About Us</Link></li>
+      <li>
+        <Link to="/about">About Us</Link>
+      </li>
 
       <li className="menu-item-has-children">
         <Link to="/service">Service</Link>
         <ul className="sub-menu">
-          <li><Link to="/service">Service</Link></li>
-          <li><Link to="/service-details">Service Details</Link></li>
+          <li>
+            <Link to="/service">Service</Link>
+          </li>
+          <li>
+            <Link to="/service-details">Service Details</Link>
+          </li>
         </ul>
       </li>
 
       <li className="menu-item-has-children">
         <Link to="#">Pages</Link>
         <ul className="sub-menu">
-          <li><Link to="/team">Team</Link></li>
-          <li><Link to="/team-details">Team Details</Link></li>
-          <li><Link to="/pricing">Pricing Plans</Link></li>
-          <li><Link to="/project">Project Gallery</Link></li>
-          <li><Link to="/project-details">Project Details</Link></li>
-          <li><Link to="/error404">Error 404 Page</Link></li>
+          <li>
+            <Link to="/team">Team</Link>
+          </li>
+          <li>
+            <Link to="/team-details">Team Details</Link>
+          </li>
+          <li>
+            <Link to="/pricing">Pricing Plans</Link>
+          </li>
+          <li>
+            <Link to="/project">Project Gallery</Link>
+          </li>
+          <li>
+            <Link to="/project-details">Project Details</Link>
+          </li>
+          <li>
+            <Link to="/error404">Error 404 Page</Link>
+          </li>
         </ul>
       </li>
 
       <li className="menu-item-has-children">
         <Link to="/blog">Blog</Link>
         <ul className="sub-menu">
-          <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/blog-grid">Blog Grid Style</Link></li>
-          <li><Link to="/blog-details">Blog Details</Link></li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/blog-grid">Blog Grid Style</Link>
+          </li>
+          <li>
+            <Link to="/blog-details">Blog Details</Link>
+          </li>
         </ul>
       </li>
 
-      <li><Link to="/contact">Contact</Link></li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
     </ul>
   );
 };
--- src/pages/HomeOne.jsx
+++ src/pages/HomeOne.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import Header from '../components/header/HeaderOne';
-import Footer from '../components/footer/FooterOne';
+import React from "react";
+import Header from "../components/header/HeaderOne";
+import Footer from "../components/footer/FooterOne";
 
-import Hero from '../components/sections/HeroOne';
-import ServiceSecOne from '../components/sections/ServiceSecOne';
-import BrandOne from '../components/sections/BrandOne';
-import AboutOne from '../components/sections/AboutOne';
-import CtaSecOne from '../components/sections/CtaSecOne';
-import CtaSecTwo from '../components/sections/CtaSecTwo';
-import WhySecOne from '../components/sections/WhySecOne';
-import CounterSecOne from '../components/sections/CounterSecOne';
-import TestimonialSecOne from '../components/sections/TestimonialSecOne';
-import TeamSecOne from '../components/sections/TeamSecOne';
+import Hero from "../components/sections/HeroOne";
+import ProcessSecOne from "../components/sections/ProcessSecOne";
+import AboutSecThree from "../components/sections/AboutSecThree";
+import ServiceSecThree from "../components/sections/ServiceSecThree";
+import WhySecThree from "../components/sections/WhySecThree";
+import ProcessSecTwo from "../components/sections/ProcessSecTwo";
+import CtaSecOne from "../components/sections/CtaSecOne";
+import TeamSecThree from "../components/sections/TeamSecThree";
+import TestiSecThree from "../components/sections/TestiSecThree";
+import CaseSecOne from "../components/sections/CaseSecOne";
+import BlogSecTwo from "../components/sections/BlogSecTwo";
+import SubscribeSecOne from "../components/sections/SubscribeSecOne";
+// import ServiceSecOne from "../components/sections/ServiceSecOne";
+// import BrandOne from "../components/sections/BrandOne";
+// import AboutOne from "../components/sections/AboutOne";
+// import CtaSecOne from "../components/sections/CtaSecOne";
+// import CtaSecTwo from "../components/sections/CtaSecTwo";
+// import WhySecOne from "../components/sections/WhySecOne";
+// import CounterSecOne from "../components/sections/CounterSecOne";
+// import TestimonialSecOne from "../components/sections/TestimonialSecOne";
+// import TeamSecOne from "../components/sections/TeamSecOne";
 
-import ProjectSecOne from '../components/sections/ProjectSecOne';
-import BlogSecTwo from '../components/sections/BlogSecTwo';
+// import ProjectSecOne from "../components/sections/ProjectSecOne";
+// import BlogSecTwo from "../components/sections/BlogSecTwo";
 
 const HomeOne = () => {
   return (
@@ -22,7 +33,18 @@ const HomeOne = () => {
       <Header />
       <div className="home-page">
         <Hero />
-        <ServiceSecOne className="service-sec1" />
+        <ProcessSecOne className="space" />
+        <AboutSecThree className="space" />
+        <ServiceSecThree className="space" />
+        <WhySecThree className="space" />
+        <ProcessSecTwo className="space-extra-top space" />
+        <CtaSecOne className="space" />
+        <TeamSecThree className="space" />
+        <TestiSecThree className="space" />
+        <CaseSecOne className="space-top" />
+        <BlogSecTwo className="space" />
+        <SubscribeSecOne className="space-bottom" />
+        {/* <ServiceSecOne className="service-sec1" />
         <AboutOne className="space" />
         <CtaSecOne className="space" />
         <WhySecOne className="space" />
@@ -32,7 +54,7 @@ const HomeOne = () => {
         <ProjectSecOne className="space-bottom" />
         <CtaSecTwo className="cta-sec2" />
         <BlogSecTwo className="space" />
-        <BrandOne className="space-bottom" />
+        <BrandOne className="space-bottom" /> */}
       </div>
       <Footer />
     </>
